360e3a9775465ae3551fc04b2c40ed8b77f168cb
SmakleAst <SmakleLover@yandex.ru>
2024-04-30 13:21:14 +0300

    Add entities

---
+++ SocialNetwork.Domain/Entity/MessageEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.Domain.Entity
+{
+    public class MessageEntity
+    {
+        public int Id { get; set; }
+        public int FromUserId { get; set; }
+        public UserEntity FromUser { get; set; }
+        public int ToUserId { get; set; }
+        public UserEntity ToUser { get; set; }
+
+        [StringLength(50)]
+        public string Header { get; set; }
+
+        [StringLength(200)]
+        public string Body { get; set; }
+        public bool IsReading { get; set; }
+        public DateTime DateOf { get; set; }
+    }
+}
---
+++ SocialNetwork.Domain/Entity/UserEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.Domain.Entity
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Login { get; set; }
+
+        [StringLength(50)]
+        public string Password { get; set; }
+
+        [StringLength(50)]
+        public string Surname { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Middlename { get; set; }
+        public List<MessageEntity> Messages { get; set; }
+    }
+}
