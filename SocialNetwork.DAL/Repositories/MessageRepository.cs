2bf882806b09cac0c99492ef829223f464a594ff
SmakleAst <SmakleLover@yandex.ru>
2024-04-30 13:39:04 +0300

    Message repository implementation

---
+++ SocialNetwork.DAL/Repositories/MessageRepository.cs
@@ -0,0 +1,44 @@
+﻿using SocialNetwork.DAL.Interfaces;
+using SocialNetwork.Domain.Entity;
+
+namespace SocialNetwork.DAL.Repositories
+{
+    public class MessageRepository : IBaseRepository<MessageEntity>
+    {
+        private AppDbContext _appDbContext;
+
+        public MessageRepository(AppDbContext appDbContext) =>
+            _appDbContext = appDbContext;
+
+        public async Task Create(MessageEntity entity)
+        {
+            await _appDbContext.Messages.AddAsync(entity);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task Delete(MessageEntity entity)
+        {
+            _appDbContext.Messages.Remove(entity);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public IQueryable<MessageEntity> GetAll()
+        {
+            return _appDbContext.Messages;
+        }
+
+        public async Task<MessageEntity> Update(MessageEntity entity)
+        {
+            _appDbContext.Messages.Update(entity);
+            await _appDbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task Attach(MessageEntity entity)
+        {
+            _appDbContext.Messages.Attach(entity);
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
