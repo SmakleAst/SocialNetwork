b86797dea811bb79ccd8fca76d41389efaaacbd3
SmakleAst <SmakleLover@yandex.ru>
2024-04-30 14:50:43 +0300

    UserService GetAllReceivedMessages implementation

--- SocialNetwork.Service/Implementations/UserService.cs
+++ SocialNetwork.Service/Implementations/UserService.cs
@@ -1,12 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using SocialNetwork.DAL.Interfaces;
+using SocialNetwork.Domain.Entity;
+using SocialNetwork.Domain.Enum;
+using SocialNetwork.Domain.Response;
+using SocialNetwork.Domain.ViewModels;
+using SocialNetwork.Service.Interfaces;
 
 namespace SocialNetwork.Service.Implementations
 {
-    internal class UserService
+    public class UserService : IUserService
     {
+        private readonly IBaseRepository<UserEntity> _userRepository;
+        private readonly IBaseRepository<MessageEntity> _messageRepository;
+
+        public UserService(IBaseRepository<UserEntity> userRepository, IBaseRepository<MessageEntity> messageRepository) =>
+            (_userRepository, _messageRepository) = (userRepository, messageRepository);
+
+        public async Task<IBaseResponse<IEnumerable<MessageViewModel>>> GetAllReceivedMessages(int userId)
+        {
+            try
+            {
+                var messages = await _messageRepository.GetAll()
+                    .Where(x => x.ToUserId == userId)
+                    .Select(x => new MessageViewModel
+                    {
+                        Id = x.Id,
+                        FromUserLogin = x.FromUser.Login,
+                        Header = x.Header,
+                        IsReading = x.IsReading,
+                        DateOf = x.DateOf.ToString("dd.MM.yyyy"),
+                    })
+                    .ToListAsync();
+
+                return new BaseResponse<IEnumerable<MessageViewModel>>
+                {
+                    Data = messages,
+                    StatusCode = StatusCode.Ok
+                };
+            }
+            catch (Exception exception)
+            {
+                return new BaseResponse<IEnumerable<MessageViewModel>>
+                {
+                    Description = exception.Message,
+                    StatusCode = StatusCode.InternalServerError
+                };
+            }
+        }
+
+        public async Task<IBaseResponse<OneMessageViewModel>> GetOneMessage(int messageId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IBaseResponse<UserViewModel>> GetUserAccountInformation(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IBaseResponse<SendMessageViewModel>> SendMessage(SendMessageViewModel model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- SocialNetwork.Service/Interfaces/IUserService.cs
+++ SocialNetwork.Service/Interfaces/IUserService.cs
@@ -6,7 +6,7 @@ namespace SocialNetwork.Service.Interfaces
     public interface IUserService
     {
         Task<IBaseResponse<UserViewModel>> GetUserAccountInformation(int userId);
-        Task<IBaseResponse<MessageViewModel>> GetAllReceivedMessages(int userId);
+        Task<IBaseResponse<IEnumerable<MessageViewModel>>> GetAllReceivedMessages(int userId);
         Task<IBaseResponse<OneMessageViewModel>> GetOneMessage(int messageId);
         Task<IBaseResponse<SendMessageViewModel>> SendMessage(SendMessageViewModel model)
     }
