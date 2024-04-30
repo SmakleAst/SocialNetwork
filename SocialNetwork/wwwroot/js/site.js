df1e0eebae82ac02b2fa3836dd77d9d0ed878fc3
SmakleAst <SmakleLover@yandex.ru>
2024-04-30 17:20:14 +0300

    Auth and Resitration form handlers implementation

--- SocialNetwork/wwwroot/js/site.js
+++ SocialNetwork/wwwroot/js/site.js
@@ -1,4 +1,72 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $('#registrationForm').submit(function (e) {
+        e.preventDefault();
+        var url = '/Auth/Registration';
 
-// Write your JavaScript code.
+        var login = $('#registrationLogin').val() || "";
+        var password = $('#registrationPassword').val() || "";
+        var surname = $('#registrationSurname').val() || "";
+        var name = $('#registrationName').val() || "";
+        var middlename = $('#registrationMiddlename').val() || "";
+
+        var formData = {
+            Login: login,
+            Password: password,
+            Surname: surname,
+            Name: name,
+            Middlename: middlename
+        };
+
+        $.ajax({
+            url: url,
+            method: 'POST',
+            data: JSON.stringify(formData),
+            contentType: 'application/json',
+            success: function (response) {
+                window.location.href = "/User/Index";
+                getUserInfo()
+            },
+            error: function (response) {
+                Swal.fire({
+                    title: response.responseJSON.description,
+                    icon: 'error',
+                    confimButtonText: 'Ok'
+                })
+            }
+        });
+    });
+
+    $('#authForm').submit(function (e) {
+        e.preventDefault();
+        var url = '/Auth/Authentification';
+
+        var login = $('#authLogin').val() || "";
+        var password = $('#authPassword').val() || "";
+
+        var formData = {
+            Login: login,
+            Password: password
+        };
+
+        $.ajax({
+            url: url,
+            method: 'GET',
+            data: formData,
+            success: function (response) {
+                window.location.href = "/User/Index";
+                getUserInfo()
+            },
+            error: function (response) {
+                Swal.fire({
+                    title: response.responseJSON.description,
+                    icon: 'error',
+                    confimButtonText: 'Ok'
+                })
+            }
+        });
+    });
+
+    function getUserInfo() {
+
+    }
+});
