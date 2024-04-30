dfa5ae0483960932e6832e669491b46d2104786d
SmakleAst <SmakleLover@yandex.ru>
2024-04-30 13:25:15 +0300

    Add base response

---
+++ SocialNetwork.Domain/Response/BaseResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace SocialNetwork.Domain.Response
+{
+    public class BaseResponse<T> : IBaseResponse<T>
+    {
+       public string Description { get; set; }
+       public T Data { get; set; }
+       public StatusCode StatusCode { get; set; }
+    }
+
+    public interface IBaseResponse<T>
+    {
+        string Description { set; }
+        T Data { set; }
+        StatusCode StatusCode { set; }
+    }
+}
