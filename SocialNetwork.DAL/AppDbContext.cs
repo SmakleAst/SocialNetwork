abb521a5a8a1bd041999946ddbf76c04fd39629f
SmakleAst <SmakleLover@yandex.ru>
2024-04-30 13:35:52 +0300

    Db context implementation

--- SocialNetwork.DAL/AppDbContext.cs
+++ SocialNetwork.DAL/AppDbContext.cs
@@ -1,6 +1,38 @@
-﻿namespace SocialNetwork.DAL
+﻿using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Domain.Entity;
+
+namespace SocialNetwork.DAL
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<MessageEntity> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Юзеры
+            modelBuilder.Entity<UserEntity>()
+                .HasMany(a => a.Messages)
+                .WithOne(a => a.FromUser)
+                .HasForeignKey(c => c.FromUserId);
+
+            modelBuilder.Entity<UserEntity>()
+                .HasMany(a => a.Messages)
+                .WithOne(a => a.ToUser)
+                .HasForeignKey(c => c.ToUserId);
+
+            //Сообщения
+            modelBuilder.Entity<MessageEntity>()
+                .HasOne(a => a.FromUser)
+                .WithMany(a => a.Messages)
+                .HasForeignKey(c => c.FromUserId);
+
+            modelBuilder.Entity<MessageEntity>()
+                .HasOne(a => a.ToUser)
+                .WithMany(a => a.Messages)
+                .HasForeignKey(c => c.ToUserId);
+        }
     }
 }
