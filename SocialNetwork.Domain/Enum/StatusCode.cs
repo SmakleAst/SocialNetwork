c9ecce667e77b488429ef0ca53e63b3aa80b5a8a
SmakleAst <SmakleLover@yandex.ru>
2024-04-30 13:26:56 +0300

    Add status code enum

---
+++ SocialNetwork.Domain/Enum/StatusCode.cs
@@ -0,0 +1,14 @@
+﻿namespace SocialNetwork.Domain.Enum
+{
+    public enum StatusCode
+    {
+        UserNotFound = 1,
+        UserAlreadyExists = 2,
+
+        MessageNotFound = 11,
+        MessageAlreadyExists = 12,
+
+        Ok = 200,
+        InternalServerError = 500,
+    }
+}
