3658ccd7822dca75d97059ce259a6bced746abae
SmakleAst <SmakleLover@yandex.ru>
2024-04-30 13:37:57 +0300

    user repository implementation

---
+++ SocialNetwork.DAL/Interfaces/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace SocialNetwork.DAL.Interfaces
+{
+    public interface IBaseRepository<T>
+    {
+        Task Create(T entity);
+        Task Delete(T entity);
+        IQueryable<T> GetAll();
+        Task<T> Update(T entity);
+        Task Attach(T entity);
+    }
+}
---
+++ SocialNetwork.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using SocialNetwork.DAL.Interfaces;
+using SocialNetwork.Domain.Entity;
+
+namespace SocialNetwork.DAL.Repositories
+{
+    public class UserRepository : IBaseRepository<UserEntity>
+    {
+        private AppDbContext _appDbContext;
+
+        public UserRepository(AppDbContext appDbContext) =>
+            _appDbContext = appDbContext;
+
+        public async Task Create(UserEntity entity)
+        {
+            await _appDbContext.Users.AddAsync(entity);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task Delete(UserEntity entity)
+        {
+            _appDbContext.Users.Remove(entity);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public IQueryable<UserEntity> GetAll()
+        {
+            return _appDbContext.Users;
+        }
+
+        public async Task<UserEntity> Update(UserEntity entity)
+        {
+            _appDbContext.Users.Update(entity);
+            await _appDbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task Attach(UserEntity entity)
+        {
+            _appDbContext.Users.Attach(entity);
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
